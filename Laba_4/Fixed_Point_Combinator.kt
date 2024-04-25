fc36648c9df2e2fd4142457d3612f20c30077fea
Viliebe <shestak7765@gmail.com>
2024-04-26 02:55:07 +0300

    ЛР4, Зад 8. Комбинатор неподвижной точки

---
+++ Laba_4/Fixed_Point_Combinator.kt
@@ -0,0 +1,10 @@
+
+fun main() {
+    val quine = """
+        fun main() {
+            val quine = \"\"\"${'$'}{""" + '"' + """}\"\"\"
+            println(quine)
+        }
+    """
+    println(quine)
+}
