7cc18ff903a9343fcf19afab74a53cf2a0758ff0
Viliebe <shestak7765@gmail.com>
2024-04-26 23:38:09 +0300

    ЛР4, Зад 8. Изменено решение комбинатора неподвижной точки

--- Laba_4/Fixed_Point_Combinator.kt
+++ Laba_4/Fixed_Point_Combinator.kt
@@ -1,10 +1,22 @@
-
-fun main() {
-    val quine = """
-        fun main() {
-            val quine = \"\"\"${'$'}{""" + '"' + """}\"\"\"
-            println(quine)
-        }
-    """
-    println(quine)
+fun main(args: Array<String>) {
+    var prog = mutableListOf(
+        "fun main(args: Array<String>) {",
+        "    var prog = mutableListOf(",
+        "    )",
+        "    println(prog[0]+\"\\n\"+prog[1])",
+        "    for(string in prog){",
+        "        println(\"\\\"\"+string+\"\\\"\")",
+        "    }",
+        "    for(i in 2..8) {",
+        "        println(prog[i])",
+        "    }",
+        "}"
+    )
+    println(prog[0]+"\n"+prog[1])
+    for(string in prog){
+        println("\""+string+"\"")
+    }
+    for(i in 2..10) {
+        println(prog[i])
+    }
 }
