3a4121d8878f25b95192829edcc4fc3c0e949471
Viliebe <shestak7765@gmail.com>
2024-05-17 06:03:29 +0300

    ЛР3, Зад 3. Добавлены предикаты для комбинаторных объектов

--- Laba_3/Kombinator_Object.pl
+++ Laba_3/Kombinator_Object.pl
@@ -43,3 +43,120 @@ build_word([Y|WordTail],[X|WordEmpty3aTail],[Y|RestWordTail]) :-
 build_3a_words(Alphabet,K,Word) :- make_pos_list(K, 0, PosList), 
 	sochet(Pos_a_List, PosList, 3), make_3a_empty_word(K, 0, Pos_a_List, WordEmpty3a), Alphabet = [a|NewAlphabet], 
 	M is K - 3, razm_povt(NewAlphabet, M, [], RestWord), build_word(Word, WordEmpty3a, RestWord).
+
+
+
+
+%Задание 4
+%Для всех комбинаторных объектов из вашего варианта построить 
+%предикат вывода всех объектов в файл.
+%ПОДМНОЖЕСТА 
+%write_subsets_to_file('num4subset.txt').
+%Предикат для генерации всех подмножеств заданного списка
+subsets([], []).
+subsets([H|T], [H|Sub]) :- subsets(T, Sub).
+subsets([_|T], Sub) :- subsets(T, Sub).
+
+% Предикат для записи всех подмножеств в файл
+write_subsets_to_file(File) :-
+    open(File, write, Stream),
+    findall(Sub, subsets([a, b, c], Sub), AllSubsets), % Генерация всех подмножеств для списка [a, b, c]
+    write_subsets(Stream, AllSubsets),
+    close(Stream).
+
+% Предикат для записи списка подмножеств в файл
+write_subsets(_, []).
+write_subsets(Stream, [H|T]) :-
+    write(Stream, H), nl(Stream),
+    write_subsets(Stream, T).
+
+
+
+%СОЧЕТАНИЯ С ПОВТОРЕНИЯМИ 
+%comb_with_rep(+List, +K, -Result)
+%write_comb_to_file('num4repeat.txt', -CombList).
+% Предикат для генерации сочетаний с повторениями
+comb_with_rep([], 0, []).
+comb_with_rep([H|T], K, [H|Comb]) :- K > 0, K1 is K - 1, comb_with_rep([H|T], K1, Comb).
+comb_with_rep([_|T], K, Comb) :- K > 0, comb_with_rep(T, K, Comb).
+
+% Предикат для записи сочетаний в файл
+write_comb_to_file(File, List) :-
+    open(File, write, Stream),
+    write_comb(Stream, List),
+    close(Stream).
+
+write_comb(_, []).
+write_comb(Stream, [Comb|T]) :-
+    write(Stream, Comb), nl(Stream),
+    write_comb(Stream, T).
+
+
+
+
+%Слова длины 5, в которых ровно одна буква повторяется 2 раза, 
+%остальные буквы не повторяются
+% Предикат для генерации слов с одним повтором
+
+% word_with_one_repeat(Word) :-
+%     alphabet(Alphabet),
+%     length(Word, 5),
+%     findall(Char, member(Char, Word), Chars),
+%     include(has_one_repeat, Chars, [RepeatChar|_]),
+%     delete(Word, RepeatChar, UniqueChars),
+%     append([RepeatChar, RepeatChar], UniqueChars, Word).
+
+% % Предикат для проверки, что символ встречается ровно 2 раза
+% has_one_repeat(Char, Word) :-
+%     count(Word, Char, 2).
+
+% % Предикат для записи слов в файл
+% write_words_to_file(File) :-
+%     open(File, write, Stream),
+%     findall(Word, word_with_one_repeat(Word), Words),
+%     write_words(Stream, Words),
+%     close(Stream).
+
+% write_words(_, []).
+% write_words(Stream, [Word|T]) :-
+%     write(Stream, Word), nl(Stream),
+%     write_words(Stream, T).
+
+% alphabet([a, b, c, d, e, f, g, h, i, j]).
+
+%Cлова длины 9, в которых ровно 2  буквы повторяются 2 раза, 
+%ровно одна буква повторяется 3 раза, остальные буквы не повторяются
+count(List, Element, Count) :-
+    sum_list(maplist((=Element), List), Count).
+    
+has_two_repeats(Char, Word) :-
+    count(Word, Char, 2).
+
+% Предикат для проверки, что символ встречается ровно 3 раза
+has_three_repeats(Char, Word) :-
+    count(Word, Char, 3).
+
+% Предикат для генерации слов, удовлетворяющих условиям
+word_with_two_two_repeats_one_three_repeat(Word) :-
+    alphabet(Alphabet),
+    length(Word, 9),
+    findall(Char, member(Char, Word), Chars),
+    select(Repeat1, Chars, RemainingChars1),
+    select(Repeat2, RemainingChars1, RemainingChars2),
+    select(Repeat3, RemainingChars2, UniqueChars),
+    append([Repeat1, Repeat1, Repeat2, Repeat2, Repeat3, Repeat3, Repeat3], UniqueChars, Word).
+
+% Предикат для записи слов в файл
+write_words_to_file(File) :-
+    open(File, write, Stream),
+    findall(Word, word_with_two_two_repeats_one_three_repeat(Word), Words),
+    write_words(Stream, Words),
+    close(Stream).
+
+write_words(_, []).
+write_words(Stream, [Word|T]) :-
+    write(Stream, Word), nl(Stream),
+    write_words(Stream, T).
+
+% Предикат для алфавита
+alphabet([a, b, c, d, e, f, g, h, i, j]).
