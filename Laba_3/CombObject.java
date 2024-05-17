6c0dcaf1f25af608bd07e77d2a18d9542bd71e8f
Viliebe <shestak7765@gmail.com>
2024-05-17 04:43:42 +0300

    ЛР3, Зад 3. Добавлен нерекурсивный метод для объекта "сочетания с повторениями"

--- Laba_3/CombObject.java
+++ Laba_3/CombObject.java
@@ -71,36 +71,77 @@ public class CombObject {
     // }
 
     //ЗАДАНИЕ 3 ВАРИАНТ 6 ОБЪЕКТ 3: ПОДМНОЖЕСТВА НЕ РЕКУРСИЯ
+    // public static void main(String[] args) {
+    //     String set = "ABCDE";
+    //     List<List<String>> allSubsets = generateSubsets(set);
+    //     writeSubsetsToFile(allSubsets);
+    // }
+
+    // public static List<List<String>> generateSubsets(String set) {
+    //     List<List<String>> subsets = new ArrayList<>();
+    //     subsets.add(new ArrayList<>());  // Add empty subset
+
+    //     for (int i = 0; i < set.length(); i++) {
+    //         int size = subsets.size();
+    //         for (int j = 0; j < size; j++) {
+    //             List<String> subset = new ArrayList<>(subsets.get(j));
+    //             subset.add(String.valueOf(set.charAt(i)));
+    //             subsets.add(subset);
+    //         }
+    //     }
+
+    //     return subsets;
+    // }
+
+    // public static void writeSubsetsToFile(List<List<String>> subsets) {
+    //     try (FileWriter writer = new FileWriter("SUBSET.txt")) {
+    //         for (List<String> subset : subsets) {
+    //             writer.write(subset.toString() + "\n");
+    //         }
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+
+    //ЗАДАНИЕ 3 ВАРИАНТ 6 ОБЪЕКТ 4: СОЧЕТАНИЯ С ПОВТОРЕНИЯМИ НЕ РЕКУРСИЯ
     public static void main(String[] args) {
-        String set = "ABCDE";
-        List<List<String>> allSubsets = generateSubsets(set);
-        writeSubsetsToFile(allSubsets);
-    }
-
-    public static List<List<String>> generateSubsets(String set) {
-        List<List<String>> subsets = new ArrayList<>();
-        subsets.add(new ArrayList<>());  // Add empty subset
-
-        for (int i = 0; i < set.length(); i++) {
-            int size = subsets.size();
-            for (int j = 0; j < size; j++) {
-                List<String> subset = new ArrayList<>(subsets.get(j));
-                subset.add(String.valueOf(set.charAt(i)));
-                subsets.add(subset);
-            }
-        }
-
-        return subsets;
-    }
-
-    public static void writeSubsetsToFile(List<List<String>> subsets) {
-        try (FileWriter writer = new FileWriter("SUBSET.txt")) {
-            for (List<String> subset : subsets) {
-                writer.write(subset.toString() + "\n");
-            }
+        int n = 5; // Количество элементов в наборе
+        int k = 3; // Максимальное количество повторений для каждого элемента
+        try {
+            generateAndWriteNonRecursiveCombinations(n, k, "", "NOREPEAT.txt");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Ошибка при записи в файл: " + e.getMessage());
         }
     }
 
+    private static void generateAndWriteNonRecursiveCombinations(int n, int k, String prefix, String fileName) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
+
+        // Инициализация массива для хранения текущего сочетания
+        int[] combination = new int[n];
+        Arrays.fill(combination, 0); // Заполняем массив нулями, чтобы обозначить отсутствие выбранного элемента
+
+        // Генерация сочетаний
+        for (int i = 0; i < Math.pow(k + 1, n); i++) {
+            StringBuilder sb = new StringBuilder(prefix);
+            for (int j = 0; j < n; j++) {
+                sb.append(combination[j]);
+            }
+            sb.append("\n");
+            writer.write(sb.toString());
+
+            // Переход к следующему сочетанию
+            int carry = 1;
+            for (int j = n - 1; j >= 0; j--) {
+                combination[j] += carry;
+                carry = 0;
+                if (combination[j] > k) {
+                    combination[j] -= k + 1;
+                    carry = 1;
+                }
+            }
+        }
+
+        writer.close();
+    }
 }
