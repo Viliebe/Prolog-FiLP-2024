e5ee8446801a64db289ae352f2f508379f324703
Viliebe <shestak7765@gmail.com>
2024-05-17 04:34:41 +0300

    ЛР3, Зад 3. Добавлен нерекурсивный метод для объекта "подмножества"

--- Laba_3/CombObject.java
+++ Laba_3/CombObject.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CombObject {
-    //ЗАДАНИЕ 3 ВАРИАНТ 6 ОБЪЕКТ 3: ПОДМНОЖЕСТВА
+    //ЗАДАНИЕ 3 ВАРИАНТ 6 ОБЪЕКТ 3: ПОДМНОЖЕСТВА РЕКУРСИЯ
 //    public static void main(String[] args) {
 //        // Исходное множество
 //        List<Integer> set = new ArrayList<>();
@@ -37,33 +37,66 @@ public class CombObject {
 //    }
 
 
-    //ЗАДАНИЕ 3 ВАРИАНТ 6 ОБЪЕКТ 3: СОЧЕТАНИЯ С ПОВТОРЕНИЯМИ
+    //ЗАДАНИЕ 3 ВАРИАНТ 6 ОБЪЕКТ 3: СОЧЕТАНИЯ С ПОВТОРЕНИЯМИ РЕКУРСИЯ
+    // public static void main(String[] args) {
+    //     String set = "ABC";
+    //     int length = 2;
+    //     List<String> combination = new ArrayList<>();
+    //     List<List<String>> allCombinations = new ArrayList<>();
+    //     generateCombinations(set, length, 0, combination, allCombinations);
+    //     writeCombinationsToFile(allCombinations);
+    // }
+
+    // private static void generateCombinations(String set, int length, int start, List<String> combination, List<List<String>> allCombinations) {
+    //     if (combination.size() == length) {
+    //         allCombinations.add(new ArrayList<>(combination));
+    //         return;
+    //     }
+
+    //     for (int i = start; i < set.length(); i++) {
+    //         combination.add(String.valueOf(set.charAt(i)));
+    //         generateCombinations(set, length, i, combination, allCombinations);
+    //         combination.remove(combination.size() - 1);
+    //     }
+    // }
+
+    // private static void writeCombinationsToFile(List<List<String>> combinations) {
+    //     try (FileWriter writer = new FileWriter("repeat.txt")) {
+    //         for (List<String> combination : combinations) {
+    //             writer.write(combination.toString() + "\n");
+    //         }
+    //     } catch (IOException e) {
+    //         e.printStackTrace();
+    //     }
+    // }
+
+    //ЗАДАНИЕ 3 ВАРИАНТ 6 ОБЪЕКТ 3: ПОДМНОЖЕСТВА НЕ РЕКУРСИЯ
     public static void main(String[] args) {
-        String set = "ABC";
-        int length = 2;
-        List<String> combination = new ArrayList<>();
-        List<List<String>> allCombinations = new ArrayList<>();
-        generateCombinations(set, length, 0, combination, allCombinations);
-        writeCombinationsToFile(allCombinations);
+        String set = "ABCDE";
+        List<List<String>> allSubsets = generateSubsets(set);
+        writeSubsetsToFile(allSubsets);
     }
 
-    private static void generateCombinations(String set, int length, int start, List<String> combination, List<List<String>> allCombinations) {
-        if (combination.size() == length) {
-            allCombinations.add(new ArrayList<>(combination));
-            return;
+    public static List<List<String>> generateSubsets(String set) {
+        List<List<String>> subsets = new ArrayList<>();
+        subsets.add(new ArrayList<>());  // Add empty subset
+
+        for (int i = 0; i < set.length(); i++) {
+            int size = subsets.size();
+            for (int j = 0; j < size; j++) {
+                List<String> subset = new ArrayList<>(subsets.get(j));
+                subset.add(String.valueOf(set.charAt(i)));
+                subsets.add(subset);
+            }
         }
 
-        for (int i = start; i < set.length(); i++) {
-            combination.add(String.valueOf(set.charAt(i)));
-            generateCombinations(set, length, i, combination, allCombinations);
-            combination.remove(combination.size() - 1);
-        }
+        return subsets;
     }
 
-    private static void writeCombinationsToFile(List<List<String>> combinations) {
-        try (FileWriter writer = new FileWriter("repeat.txt")) {
-            for (List<String> combination : combinations) {
-                writer.write(combination.toString() + "\n");
+    public static void writeSubsetsToFile(List<List<String>> subsets) {
+        try (FileWriter writer = new FileWriter("SUBSET.txt")) {
+            for (List<String> subset : subsets) {
+                writer.write(subset.toString() + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
