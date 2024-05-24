bb4742405f9311d14772d3ebdc54f97211cbf0ee
Viliebe <shestak7765@gmail.com>
2024-05-24 04:50:01 +0300

    ЛР5, Зад 7. Добавлено решение красивых чисел

---
+++ Laba_5/src/PrettyNum.kt
@@ -0,0 +1,22 @@
+fun factorial(n: Int): Int {
+    return if (n == 0) 1 else n * factorial(n - 1)
+}
+
+fun sumOfFactorialDigits(n: Int): Int {
+    return if (n == 0) 0 else factorial(n % 10) + sumOfFactorialDigits(n / 10)
+}
+
+fun isCuriousNumber(n: Int): Boolean {
+    return n == sumOfFactorialDigits(n)
+}
+
+fun findCuriousNumbersSum(limit: Int): Int {
+    return (10 until limit)
+        .filter(::isCuriousNumber)
+        .sum()
+}
+
+fun main() {
+    val result = findCuriousNumbersSum(100000)
+    println(result)
+}
